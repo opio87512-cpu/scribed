513923375724660005e9c28d510661b42c68a618
opio87512-cpu <opio87512@gmail.com>
2026-08-25 13:12:22 +0300

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import yt_dlp
 from flask import Flask
 
-# Your Telegram Token
+# Updated Telegram Bot Token
 BOT_TOKEN = "8969647277:AAF3jTCal-ZdqYqghm7ln0mrcTZUcTg3o6U"
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -50,7 +50,7 @@ def handle_tiktok_link(message):
                 
                 # Send the album directly to the user
                 bot.send_media_group(message.chat.id, media_group)
-                bot.delete_message(message.chat.id, status_msg.message_id)
+                bot.delete_message(chat_id=message.chat.id, message_id=status_msg.message_id)
                 return  # Stop the function here so it doesn't try to download a video
 
         # Step 2: If it's NOT a photo post, fallback to yt-dlp for video extraction
@@ -76,7 +76,7 @@ def handle_tiktok_link(message):
                     video=video_file,
                     supports_streaming=True
                 )
-            bot.delete_message(chat_id=message.chat.id, status_msg.message_id)
+            bot.delete_message(chat_id=message.chat.id, message_id=status_msg.message_id)
             os.remove(file_name)
         else:
             bot.edit_message_text("❌ Error: Could not download the video.", chat_id=message.chat.id, message_id=status_msg.message_id)
